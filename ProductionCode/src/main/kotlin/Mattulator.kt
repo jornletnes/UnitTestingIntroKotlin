312658f10c27d67a411488a0b3a61b4f2692e78c
jorn.letnes@gmail.com <jorn.letnes@gmail.com>
2022-03-20 10:30:02 +0100

    first pass

---
+++ ProductionCode/src/main/kotlin/Mattulator.kt
@@ -0,0 +1,17 @@
+class Mattulator {
+    fun Adder(tall1: Double, tall2: Double): Double {
+        return tall1 + tall2
+    }
+
+    fun Subtraher(tall1: Double, tall2: Double): Double {
+        return tall1 - tall2
+    }
+
+    fun Multipliser(tall1: Double, tall2: Double): Double {
+        return tall1 * tall2
+    }
+
+    fun Divider(teller: Double, nevner: Double): Double {
+        return nevner / teller
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # UnitTestingIntroKotlin
+
+En kjapp innføring i testing. Repoet inneholder noe eksempel-produksjonskode. Oppgaven er å skrive Arrange / Act / Assert tester mot Mattulator-klassen.
